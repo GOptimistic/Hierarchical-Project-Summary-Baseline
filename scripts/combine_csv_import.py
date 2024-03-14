84cae3d62f68a1bc5fbcaea4b5ea65dcec9cc3cf
GOptimistic <969935802@qq.com>
2024-03-14 13:38:18 +0800

    fix unmaintain

--- scripts/combine_csv_import.py
+++ scripts/combine_csv_import.py
@@ -99,7 +99,7 @@ def handle_csv():
             repo_name = row['repo_name']
             files_info = row['files_info']
             repo_summary = ' '.join(row['repo_summary'].split())
-            if not re.match('\$', repo_summary) and not re.match(filter_pattern, flags=re.IGNORECASE):
+            if not re.match(r'\$', repo_summary) and not re.match(filter_pattern, repo_summary, flags=re.IGNORECASE):
                 flat_result_list.append([repo_name, get_project_flat_input(repo_name, files_info), repo_summary])
             row['files_info'] = handle_files_info(files_info)
             row['repo_summary'] = repo_summary
